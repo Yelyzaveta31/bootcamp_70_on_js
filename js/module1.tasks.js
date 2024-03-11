3bfd3f2250e70ccfa6a1e44435ef712c327c4086
Yelyzaveta31 <Didenkoelizabeth@gmail.com>
2024-03-11 21:37:04 +0200

    module.1.tasks

--- js/module1.tasks.js
+++ js/module1.tasks.js
@@ -1,8 +1,3 @@
-/*
- * Арифметичні операції
- * - Оператори +, -, *, /, %, **
- * - Комбіновані оператори
- */
 
 
 /*
@@ -43,11 +38,62 @@ console.log(result);
   ? Результат виведіть у консоль.
 */
 
+const totalMinutes = 90;
+const hours = Math.floor(totalMinutes / 60);
+const minutes = totalMinutes % 60;
 
 
-// const totalMinutes = 90;
-// const hours = Math.floor(totalMinutes / 60);
-// const minutes = totalMinutes % 60;
 
-// console.log(hours);
-// console.log(minutes);
+console.log(hours);
+ console.log(minutes);
+
+
+ //? Обʼєднайте два рядки в один і виведіть до консолі.
+
+const firstName = 'Chelsy';
+const lastName = 'Emerald';
+
+console.log(firstName + " " + lastName)
+
+
+//? Підставте значення зміної quantity до рядка orderMsg, використоуючи шаблоні рядки.
+
+const quantity = 15;
+const orderMsg = `You ordered ${quantity} bots`;
+console.log(orderMsg);
+
+/*
+  ? Запросіть у користувача ім'я та прізвище і виведіть у консоль повідомлення:
+  ? Hello <імʼя> <прізвище>, you are logged in.
+*/
+
+const firstNamee = prompt("What is your name?");
+const lastNamee = prompt("What is your last name?");
+const message = `Hello ${firstNamee} ${lastNamee}, you are logged in.`
+console.log(message);
+
+
+//? Дізнайтесь довжину рядка message.
+
+const messagee = 'This string is 28 chars long';
+console.log(messagee.length);
+
+// console.log(message.length);
+
+// console.log('Hello'.length);
+
+/*
+ ? Склади фразу за допомогою шаблонних рядків
+ ? A has B bots in stock, де A, B - змінні вставлені в рядок.
+ */
+
+
+ const companyName = 'Cyberdyne Systems';
+const repairBots = 150;
+const defenceBots = 50;
+const totalBots = repairBots + defenceBots;
+const msg = `${companyName} has ${totalBots} bots in stock`;
+console.log(msg);
+// const msg = `${companyName} has ${repairBots + defenceBots} bots in stock`;
+
+// console.log(msg); // "Cyberdyne Systems has 200 bots in stock"
