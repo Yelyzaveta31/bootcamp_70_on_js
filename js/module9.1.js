627ebcf1e14aabe25aa86294b187faeb0b13917c
Yelyzaveta31 <Didenkoelizabeth@gmail.com>
2024-04-03 15:28:54 +0300

    update module9.1

--- js/module9.1.js
+++ js/module9.1.js
@@ -46,8 +46,48 @@ addKeyToLocalStorage(students)
 // Створіть функцію, 
 // яка буде видаляти всі значення з localStorage.
 
-function clearAllToLocalStorage() {
-    localStorage.clear()
-}
-clearAllToLocalStorage();
+// function clearAllToLocalStorage() {
+//     localStorage.clear()
+// }
+// clearAllToLocalStorage();
 
+
+// Створіть функцію, 
+// яка буде повертати кількість елементів в localStorage
+
+
+const student = {
+    studentName: "Mary",
+    block: "Node",
+    duration: 7
+};
+
+localStorage.setItem("StudentSecond", JSON.stringify(student));
+console.log(localStorage);
+
+function lengthLocalStorage() {
+    return localStorage.length
+};
+console.log(lengthLocalStorage());
+
+// Створіть функцію,
+//  яка буде повертати масив ключів з localStorage
+
+function getArrayKeys() {
+    return Object.keys(localStorage);
+}
+console.log(getArrayKeys());
+
+
+// Створіть функцію, яка буде перевіряти, 
+// чи існує об'єкт в localStorage за ключем.
+
+const checkKey = () => {
+    const key = localStorage.getItem("studentName")
+    if (checkKey === null) {
+        return `Such key does not exist`
+    } 
+        return checkKey;
+}
+
+console.log(checkKey());
